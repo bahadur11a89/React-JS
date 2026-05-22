bbbd899968cce57b1341fa5b9c42b437b97e31e2
Raj Bahadur <bahadur11a89@gmail.com>
2026-05-22 10:18:57 +0530

    code update

---
+++ ReactApna/miniproject/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import SearchBox from './SearchBox.jsx'
+
+const App = () => {
+  return (
+    <>
+      <SearchBox/>
+    </>
+  )
+}
+
+export default App
--- ReactApna/miniproject/src/SearchBox.jsx
+++ ReactApna/miniproject/src/SearchBox.jsx
@@ -1,12 +1,43 @@
+import { useState } from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
+import "./SearchBox.css";
 
 export default function BasicTextFields() {
+
+  const [city, setCity] = useState('Raipur City');
+
   return (
-    <Box>
-      <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-      <TextField id="filled-basic" label="Filled" variant="filled" />
-      <TextField id="standard-basic" label="Standard" variant="standard" />
+    <Box className='SearchBox'>
+
+      <TextField 
+        id="outlined-basic" 
+        label="Outlined" 
+        variant="outlined" 
+        value={city}
+        onChange={(e) => setCity(e.target.value)}
+      /> 
+
+      <br /><br />
+
+      <TextField 
+        id="filled-basic" 
+        label="Filled" 
+        variant="filled" 
+      /> 
+
+      <br /><br />
+
+      <TextField 
+        id="standard-basic" 
+        label="Standard" 
+        variant="standard" 
+      /> 
+
+      <br /><br />
+
+      <button type='submit'>Search</button>
+
     </Box>
   );
 }
